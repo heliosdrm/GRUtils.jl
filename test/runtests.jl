8c167459724734b94b052c3936e6cf8052055e10
Helios De Rosario <helios.derosario@gmail.com>
2019-09-26 16:24:41 +0200

    ommit tests with gr3

--- test/runtests.jl
+++ test/runtests.jl
@@ -170,12 +170,12 @@ x = [randn(10000); NaN; randn(10000) .+ 5 ]
 y = [randn(10000); NaN; randn(10000) .+ 5]
 shade(x, y, xform=3)
 
-# Create example data
-x = LinRange(-1, 1, 40)
-y = LinRange(-1, 1, 40)
-z = LinRange(-1, 1, 40)
-v = 1 .- (x.^2 .+ y'.^2 .+ reshape(z,1,1,:).^2).^0.5 - 0.25 .* rand(40, 40, 40)
-# Draw the 3d volume data
-volume(v)
-# Draw the 3d volume data using an emission model
-volume(v, algorithm=2)
+# # Create example data
+# x = LinRange(-1, 1, 40)
+# y = LinRange(-1, 1, 40)
+# z = LinRange(-1, 1, 40)
+# v = 1 .- (x.^2 .+ y'.^2 .+ reshape(z,1,1,:).^2).^0.5 - 0.25 .* rand(40, 40, 40)
+# # Draw the 3d volume data
+# volume(v)
+# # Draw the 3d volume data using an emission model
+# volume(v, algorithm=2)
