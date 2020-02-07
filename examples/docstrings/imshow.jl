024ef866710c8ffddf8fd176d0785629cc991cae
Helios De Rosario <helios.derosario@gmail.com>
2020-02-07 18:59:42 +0100

    new imshow example

--- examples/docstrings/imshow.jl
+++ examples/docstrings/imshow.jl
@@ -2,9 +2,9 @@
 x = LinRange(-3, 3, 150)
 y = LinRange(-2, 2, 100)
 # RGB values
-r = ones(100) * (1 .+ cos.(x)')/2
-g = (1 .+ sin.(exp.(x'.^2 .+ y.^2)))/2
-b = exp.(y .- 2) * ones(1,150)
+r = (1 .+ cos.(atan.(y, x')))/2
+g = (1 .+ sin.(atan.(y, x')))/2
+b = exp.(-(x'.^2 .+ y.^2)/4)
 data = cat(r, g, b, dims=3)
 # Draw the image
 imshow(data)
